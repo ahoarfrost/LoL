64f3bd639b478a88d16b6a69fa1fb4dd2581257c
ahoarfrost <adrienne.l.hoarfrost@gmail.com>
2020-05-29 15:36:25 -0400

    add train lm pre-final training

---
+++ TrainLM/train_gpu_trainlm.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#SBATCH --partition=gpu                       # Partition (job queue)
+#SBATCH --gres=gpu:2
+#SBATCH --constraint=pascal
+#SBATCH --job-name=trainlm                       # Assign an short name to your job
+#SBATCH --nodes=1                                   # Number of nodes you require
+#SBATCH --ntasks-per-node=28
+#SBATCH --mem=128000
+#SBATCH --time=3-00:00:00                           # Total run time limit (HH:MM:SS)
+#SBATCH --output=slurm.train_GTDB_read_LM_tuned.out     # STDOUT output file
+#SBATCH --export=ALL                                # Export you current env to the job env
+
+
+python -m torch.distributed.launch --nproc_per_node=2 train_GTDB_read_LM.py --n_cpus 28
