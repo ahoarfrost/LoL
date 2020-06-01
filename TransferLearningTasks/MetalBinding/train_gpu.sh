828cd54532d8606257e262ae5819dd1ca2f6e87c
ahoarfrost <adrienne.l.hoarfrost@gmail.com>
2020-06-01 12:50:36 -0400

    first run metal binding classifier training

---
+++ TransferLearningTasks/MetalBinding/train_gpu.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#SBATCH --partition=gpu                       # Partition (job queue)
+#SBATCH --gres=gpu:2
+#SBATCH --constraint=pascal
+#SBATCH --job-name=trnmet                       # Assign an short name to your job
+#SBATCH --nodes=1                                   # Number of nodes you require
+#SBATCH --ntasks-per-node=28
+#SBATCH --mem=128000
+#SBATCH --time=3-00:00:00                           # Total run time limit (HH:MM:SS)
+#SBATCH --output=slurm.train_metalbinding_clas.out     # STDOUT output file
+#SBATCH --export=ALL                                # Export you current env to the job env
+
+
+python -m torch.distributed.launch --nproc_per_node=2 TrainMetalBinding.py --n_cpus 28
